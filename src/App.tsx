f33244d45ffe7c579293e0e3e2134fd538a4b254
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-12-06 17:23:50 -0300

    feat: Import RoutesApp component on App component
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import { Card } from './components/Countries/Card';
-import { Header } from './components/Header';
+import { RoutesApp } from './routes';
 
 export function App() {
-  return (
-    <>
-      <Header />
-      <Card />
-    </>
-  );
+  return <RoutesApp />;
 }
