24308da7b992d7250f944748054d4c5cc25b5433
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-12-12 14:55:06 -0300

    feat: Filter Countries By Region components
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

---
+++ src/components/FilterCountriesByRegion.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { Card, CountriesCard } from './Countries/Card';
+
+interface FilterCountriesByRegionProps {
+  countries: CountriesCard[];
+}
+
+export function FilterCountriesByRegion({
+  countries,
+}: FilterCountriesByRegionProps) {
+  const [selectedRegion, setSelectedRegion] = useState<string>('');
+
+  const handleRegionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedRegion(e.target.value);
+  };
+
+  const filteredCountries = selectedRegion
+    ? countries.filter((country) => country.region === selectedRegion)
+    : [];
+
+  return (
+    <div>
+      <select id="regionSelect" onChange={handleRegionChange}>
+        <option value="">Select Region</option>
+        <option value="Africa">Africa</option>
+        <option value="Americas">Americas</option>
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="Oceania">Oceania</option>
+      </select>
+
+      {selectedRegion && (
+        <div>
+          <h3>Countries in {selectedRegion}</h3>
+          <Card countries={filteredCountries} />
+        </div>
+      )}
+    </div>
+  );
+}
