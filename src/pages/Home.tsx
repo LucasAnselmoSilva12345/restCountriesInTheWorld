4fdd02a4d78cfa2fd5e715434a9e94c52257b852
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-12-12 16:03:10 -0300

    feat: Adding search countries on main homepage
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,30 @@
 import dataCountriesJSON from '../lib/data.json';
-import { Search } from '../components/Search';
-import { FilterCountriesByRegion } from '../components/FilterCountriesByRegion';
+import { useState } from 'react';
+import { Input } from '../components/ui/input';
+import { Card } from '../components/Countries/Card';
 
 export function Home() {
   const countries = dataCountriesJSON;
+
+  const [search, setSearch] = useState<string>('');
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  };
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <section className="p-6 lg:px-16">
-      <FilterCountriesByRegion countries={countries} />
-      <Search countries={countries} />
+      <Input
+        type="text"
+        placeholder="Search countries..."
+        value={search}
+        onChange={handleSearch}
+      />
+      <Card countries={filteredCountries} />
     </section>
   );
 }
