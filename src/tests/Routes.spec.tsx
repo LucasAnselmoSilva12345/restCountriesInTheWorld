538122380846f89dc2985b00df3b0e991b9e3c8d
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-12-12 15:52:28 -0300

    test: Check Filter by region route
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/tests/Routes.spec.tsx
+++ src/tests/Routes.spec.tsx
@@ -1,22 +1,40 @@
 import { screen, render } from '@testing-library/react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import { Home } from '../pages/Home';
+import { FilterCountries } from '../pages/FilterCountries';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/filter-by-region" element={<FilterCountries />} />
+    </Routes>
   );
 };
 
 describe('Router tests', () => {
   it('should render homepage', () => {
-    render(<App />);
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    );
+
     const afghanistanCountry = screen.getByText(/Afghanistan/i);
     expect(afghanistanCountry).toBeInTheDocument();
+  });
+
+  it('should render FilterCountries component when navigating to /filter-by-region', () => {
+    const filterByRegionRoute = '/filter-by-region';
+    render(
+      <MemoryRouter initialEntries={[filterByRegionRoute]}>
+        <App />
+      </MemoryRouter>
+    );
+
+    const filterCountriesHeading = screen.getByText(
+      /Filter countries based on Region/i
+    );
+    expect(filterCountriesHeading).toBeInTheDocument();
   });
 });
