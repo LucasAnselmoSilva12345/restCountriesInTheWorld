35e9a5d009331f5f6490afc519932304c7fb8c8f
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-12-16 16:59:52 -0300

    feat: Link to the homepage on Title app in Header component
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,8 +18,8 @@ export function Header() {
           </ul>
         </nav>
 
-        <h1 className="font-semibold text-xl text-foreground">
-          Where in the world?
+        <h1 className="font-semibold text-2xl lg:text-4xl text-foreground">
+          <Link to="/">Where in the world?</Link>
         </h1>
 
         <ModeToggle />
